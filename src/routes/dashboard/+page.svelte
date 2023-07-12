4966a33650f3be4de2e723da4a9e5952ec094f18
haveesh-s <131840416+haveesh-s@users.noreply.github.com>
2023-07-12 15:57:34 +1000

    different dashboard for each account type
    get user document from firestore and conditionally render dashboard based on account type. also updated type checking and loading font.

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,10 +4,13 @@
   import { firebaseAuth, db } from '$lib/firebase';
   import { doc, getDoc } from 'firebase/firestore';
 
-  let accountType: string | undefined = undefined;
+  let accountType: string | undefined | null = undefined;
   let user = firebaseAuth.currentUser!;
 
-  getDoc(doc(db, "users", user.uid))
+  getDoc(doc(db, "users", user.uid)).then(document => {
+    if (document.exists()) { accountType = document.get('type'); }
+    else { accountType = null; }
+  })
 </script>
 
 <svelte:head>
@@ -18,14 +21,16 @@
 
 <div class="main">
   <h2 class="text-4xl">Welcome, {user.displayName}</h2>
-  {#if accountType =='student'}
+  {#if accountType === 'student'}
     <h1>Student Dash</h1>
     <h2>Placeholder</h2>
-  {:else if accountType =='teacher'}
+  {:else if accountType === 'teacher'}
     <h1>Teacher Dash</h1>
     <h2>Placeholder</h2>
+  {:else if accountType === null}
+    <h1>We ran into an error!</h1>
   {:else}
     <Loading style="h-24"/>
-    <h1>Loading dashboard...</h1>
+    <h2>Loading dashboard...</h2>
   {/if}
 </div>
