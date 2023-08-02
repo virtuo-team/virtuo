736e901d87412fa88df224383e99fc0ccaefec91
real-yifei <139315198+real-yifei@users.noreply.github.com>
2023-08-02 14:01:08 +1000

    started working on teacher register
    ran out of time

--- src/lib/components/dashboards/Student.svelte
+++ src/lib/components/dashboards/Student.svelte
@@ -7,7 +7,7 @@
     let teacherEmail: string;
 
     const teacherRegister = () => {
-        console.log('coming soon yoyfs baguette');
+        console.log('haveesh says he will do this at home to be continued...');
     }
 </script>
 
@@ -25,6 +25,10 @@
             <div class="flex gap-2 mb-4">
                 <input name="teacherEmail" type="email" placeholder="Teacher's Email" required bind:value={teacherEmail}
                     class="rounded-2xl mx-auto p-2 w-96 bg-_eerieblack border-_eerieblack border-2 focus:outline-none focus:border-_celestialblue"/>
+                <button class="rounded-2xl mx-auto bg-_cerise px-4 py-2" type="submit">
+                    Submit
+                </button>
+                    
             </div>
         </form>
     </div>
